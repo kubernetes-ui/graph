5447a1ced95dacc8bf9aa5865757353a4cae1d08
jackgr <jackgr@google.com>
2015-02-08 20:05:56 -0800

    Renamed the graph-visualizer folder to match the route. Added chrome for the tab.

---
+++ js/modules/controllers/graph.js
@@ -0,0 +1,12 @@
+/**=========================================================
+ * Module: Graph
+ * Visualizer for force directed graph
+ =========================================================*/
+(function() {
+  'use strict';
+
+  angular.module('krakenApp.Graph')
+    .controller('GraphCtrl', ['$scope', function($scope) {
+    }]);
+
+})();
---
+++ js/modules/services/viewModelService.js
@@ -0,0 +1,15 @@
+/**=========================================================
+ * Module: Graph
+ * Visualizer for force directed graph
+ =========================================================*/
+(function() {
+  'use strict';
+
+  angular.module('krakenApp.Graph')
+    .service('viewModelService', ['$scope', ViewModelService]);
+
+  var viewModelService = function ViewModelService($scope) {
+  };
+
+})();
+
