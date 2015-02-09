f0dfa37bfaac1bd1ba2b07c9554dc16476b0400b
duftler <duftler@google.com>
2015-02-09 17:49:40 -0500

    Remove hard-coded demo data from d3-visualization directive. Remove double-click -> cycle through demo data behavior. Retrieve demo data from scope.viewModelService.Clustered.

--- js/modules/services/viewModelService.js
+++ js/modules/services/viewModelService.js
@@ -66,7 +66,142 @@
 		            "showEdgeLabels" : true,
 		            "showNodeLabels" : true
 		        }
-    		}}
+    		},
+			"AllServices": {
+				"nodes": [
+				  {
+					"name": "svc: guestbook",
+					"group": 1,
+					"radius": 16
+				  }, {
+					"name": "pod: guestbook-controller",
+					"group": 2,
+					"radius": 20
+				  }, {
+					"name": "pod: guestbook-controller",
+					"group": 2,
+					"radius": 20
+				  }, {
+					"name": "pod: guestbook-controller",
+					"group": 2,
+					"radius": 20
+				  }, {
+					"name": "container: php-redis",
+					"group": 3,
+					"radius": 24
+				  }, {
+					"name": "container: php-redis",
+					"group": 3,
+					"radius": 24
+				  }, {
+					"name": "container: php-redis",
+					"group": 3,
+					"radius": 24
+				  }, {
+					"name": "svc: redis-master",
+					"group": 1,
+					"radius": 16
+				  }, {
+					"name": "pod: redis-master",
+					"group": 2,
+					"radius": 20
+				  }, {
+					"name": "container: master",
+					"group": 3,
+					"radius": 24
+				  }, {
+					"name": "lb: guestbook",
+					"group": 4,
+					"radius": 16
+				  }, {
+					"name": "svc: redis-worker",
+					"group": 1,
+					"radius": 16
+				  }, {
+					"name": "pod: redis-worker-controller",
+					"group": 2,
+					"radius": 20
+				  }, {
+					"name": "container: slave",
+					"group": 3,
+					"radius": 24
+				  }, {
+					"name": "container: slave",
+					"group": 3,
+					"radius": 24
+				  }
+				],
+				"links": [
+				  {
+					"source": 0,
+					"target": 1,
+					"thickness": 2,
+					"distance": 160
+				  }, {
+					"source": 0,
+					"target": 2,
+					"thickness": 2,
+					"distance": 160
+				  }, {
+					"source": 0,
+					"target": 3,
+					"thickness": 2,
+					"distance": 160
+				  }, {
+					"source": 1,
+					"target": 4,
+					"thickness": 1,
+					"distance": 240
+				  }, {
+					"source": 2,
+					"target": 5,
+					"thickness": 1,
+					"distance": 240
+				  }, {
+					"source": 3,
+					"target": 6,
+					"thickness": 1,
+					"distance": 240
+				  }, {
+					"source": 7,
+					"target": 8,
+					"thickness": 2,
+					"distance": 160
+				  }, {
+					"source": 8,
+					"target": 9,
+					"thickness": 1,
+					"distance": 240
+				  }, {
+					"source": 10,
+					"target": 0,
+					"thickness": 3,
+					"distance": 80,
+					"label": "port: 3000"
+				  }, {
+					"source": 11,
+					"target": 12,
+					"thickness": 2,
+					"distance": 160
+				  }, {
+					"source": 12,
+					"target": 13,
+					"thickness": 1,
+					"distance": 240
+				  }, {
+					"source": 12,
+					"target": 14,
+					"thickness": 1,
+					"distance": 240
+				  },
+				],
+				"settings": {
+				  "clustered": false,
+				  "showEdgeLabels": true,
+				  "showNodeLabels": true
+				}
+			}
+		  }
 		};
 	};
 
