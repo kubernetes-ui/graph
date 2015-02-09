1b47d77945ca81cc3c54440e7034793e021fa6fc
jackgr <jackgr@google.com>
2015-02-09 12:24:21 -0800

    Filled in GraphCtrl and viewModelService with hard wired data.

--- js/modules/controllers/graph.js
+++ js/modules/controllers/graph.js
@@ -3,10 +3,12 @@
  * Visualizer for force directed graph
  =========================================================*/
 (function() {
-  'use strict';
+	'use strict';
 
-  angular.module('krakenApp.Graph', [])
-    .controller('GraphCtrl', ['$scope', function($scope) {
+	angular.module('krakenApp.Graph', [])
+    .controller('GraphCtrl', ['$scope', 'viewModelService', function($scope, viewModelService) {
+    	// Watch the control parameters and the data model, and call the viewModelService when they change.
+    	// For now, just call the viewModelService once.
+    	$scope.viewModel = viewModelService($scope);
     }]);
-
 })();
--- js/modules/services/viewModelService.js
+++ js/modules/services/viewModelService.js
@@ -3,13 +3,75 @@
  * Visualizer for force directed graph
  =========================================================*/
 (function() {
-  'use strict';
+	'use strict';
 
-  angular.module('krakenApp.Graph')
-    .service('viewModelService', ['$scope', viewModelService]);
+	angular.module('krakenApp.Graph')
+	.service('viewModelService', ['$scope', viewModelService]);
 
-  var viewModelService = function ViewModelService($scope) {
-  };
+	// Compute the view model based on the data model and control parameters
+	// and place the result in the current scope at $scope.viewModel.
+	var viewModelService = function ViewModelService($scope) {
+		// For now, just hardwire the view model contents.
+	  	return {
+  			"Clustered" : { 
+  				"nodes" : [ 
+	  			  { "group" : 1,
+		            "name" : "pod: guestbook-controller",
+		            "radius" : 24
+		          },
+		          { "group" : 2,
+		            "name" : "pod: guestbook-controller",
+		            "radius" : 24
+		          },
+		          { "group" : 3,
+		            "name" : "pod: guestbook-controller",
+		            "radius" : 24
+		          },
+		          { "group" : 1,
+		            "name" : "container: php-redis",
+		            "radius" : 20
+		          },
+		          { "group" : 2,
+		            "name" : "container: php-redis",
+		            "radius" : 20
+		          },
+		          { "group" : 3,
+		            "name" : "container: php-redis",
+		            "radius" : 20
+		          },
+		          { "group" : 4,
+		            "name" : "pod: redis-master",
+		            "radius" : 24
+		          },
+		          { "group" : 4,
+		            "name" : "container: master",
+		            "radius" : 20
+		          },
+		          { "group" : 5,
+		            "name" : "pod: redis-worker-controller",
+		            "radius" : 24
+		          },
+		          { "group" : 5,
+		            "name" : "container: slave",
+		            "radius" : 20
+		          },
+		          { "group" : 5,
+		            "name" : "container: slave",
+		            "radius" : 20
+		          }
+		        ],
+			    "settings" : { 
+			    	"clusterSettings" : { 
+			    		"clusterPadding" : 25,
+		                "padding" : 1.5
+		            },
+		            "clustered" : true,
+		            "showEdgeLabels" : true,
+		            "showNodeLabels" : true
+		        }
+    		}
+		};
+	};
 
 })();
 
