2d2245e96e681235690def4335476dd8f188ef15
Xin Zhang <xinzh@google.com>
2015-02-10 11:38:06 -0500

    Wire the k8s data polling service with the view model service, so that the latter can make data projections based on the k8s data. Tested it locally and the polling can get started and proceed, with the known auth issue we have been talking about (plan to add the CORS header in followup RP)
    Also defined a bunch of constants to be used in the polling service,
    however, I am still figuring out how to reference them from the
    polling service.

--- js/modules/controllers/graph.js
+++ js/modules/controllers/graph.js
@@ -5,10 +5,16 @@
 (function() {
 	'use strict';
 
-	angular.module('krakenApp.Graph', [])
-    .controller('GraphCtrl', ['$scope', 'viewModelService', function($scope, viewModelService) {
+	angular.module('krakenApp.Graph', ['krakenApp.services'])
+    .controller('GraphCtrl', ['$scope', 'viewModelService', 'pollK8sDataService',
+                              function($scope, viewModelService, pollK8sDataService) {
     	// Watch the control parameters and the data model, and call the viewModelService when they change.
     	// For now, just call the viewModelService once.
+
+        // We can get the latest data model by calling pollK8sDataService.k8sdatamodel.data,
+        // which can then be passed into viewModelService for the projection (TODO).
+        var dataModel = pollK8sDataService.k8sdatamodel.data;
+
     	$scope.viewModel = viewModelService.data;
     }]);
 })();
