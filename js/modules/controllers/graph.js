67c0b1e6e9d4f579baf438fd6934f44744535c38
Xin Zhang <xinzh@google.com>
2015-02-10 14:02:15 -0500

    revert changes in graph.js

--- js/modules/controllers/graph.js
+++ js/modules/controllers/graph.js
@@ -5,16 +5,10 @@
 (function() {
 	'use strict';
 
-	angular.module('krakenApp.Graph', ['krakenApp.services'])
-    .controller('GraphCtrl', ['$scope', 'viewModelService', 'pollK8sDataService',
-                              function($scope, viewModelService, pollK8sDataService) {
+	angular.module('krakenApp.Graph', [])
+    .controller('GraphCtrl', ['$scope', 'viewModelService', function($scope, viewModelService) {
     	// Watch the control parameters and the data model, and call the viewModelService when they change.
     	// For now, just call the viewModelService once.
-
-        // We can get the latest data model by calling pollK8sDataService.k8sdatamodel.data,
-        // which can then be passed into viewModelService for the projection (TODO).
-        var dataModel = pollK8sDataService.k8sdatamodel.data;
-
-    	$scope.viewModel = viewModelService;
+    	$scope.viewModel = viewModelService.data;
     }]);
 })();
