3f831d53c3f860134a3e5bbd28b8a55b24fe6a3f
xinzh-mac <xinzh@google.com>
2015-02-21 21:11:19 -0500

    Remove the need of another lodash

--- js/modules/controllers/inspectNode.js
+++ js/modules/controllers/inspectNode.js
@@ -7,10 +7,10 @@
 
   angular.module('krakenApp.Graph').controller(
       'InspectNodeCtrl',
-      ['$scope', 'inspectNodeService', function($scope, inspectNodeService) {
-        $scope.nodeDetail = JSON.stringfy(inspectNodeService.getDetailData(), undefined, 2);
-        console.log('The node detail is ' + $scope.nodeDetail);
+      ['$scope', 'inspectNodeService', 'lodash', function($scope, inspectNodeService, lodash) {
+        // TODO(xinzh): When the data server is up and we have live data, only display
+        // the metadata section in the detailed data.
+        $scope.nodeDetail = JSON.stringify(inspectNodeService.getDetailData(), undefined, 2);
   }]);
-
 
 })();
