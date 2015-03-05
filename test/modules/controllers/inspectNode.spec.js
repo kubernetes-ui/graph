c7f2af4430597167c5584a406ccc45d24f1ee795
xinzh-mac <xinzh@google.com>
2015-03-05 16:02:23 -0500

    unittest for inspect node controller

---
+++ test/modules/controllers/inspectNode.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe("Inspect node controller", function() {
+  var inspectNodeService = {};
+  var scope, location, controller;
+  var mockNodeDetail = {'id': 1, 'metadata': 'data'};
+  // Work around to get ngLodash correctly injected.
+  beforeEach(function () {
+    angular.module('testModule',
+                   ['ngLodash', 'krakenApp.Graph']);
+  });
+
+  beforeEach(module('testModule'));
+
+  beforeEach(inject(function (
+      _inspectNodeService_, $controller, $location, $rootScope) {
+    inspectNodeService = _inspectNodeService_;
+    // Mock the node detail data returned by the service
+    inspectNodeService.setDetailData(mockNodeDetail);
+    scope = $rootScope.$new();
+    location = $location;
+    controller = $controller('InspectNodeCtrl', {
+      $scope: scope,
+      $location: location
+    });
+  }));
+
+  it("should work as intended", function() {
+    // Test if the controller sets the correct model values.
+    expect(scope.element).toEqual(mockNodeDetail.id);
+    expect(scope.metadata).toEqual(mockNodeDetail.metadata);
+
+    // Test if the controller change the location correctly.
+    scope.backToGraph();
+    expect(location.path()).toEqual('/graph');
+  });
+});
