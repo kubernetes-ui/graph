14d7dde103a094cfdaa526cf71f2facc39d99868
duftler <duftler@google.com>
2015-03-08 11:50:56 -0400

    Tweak some formatting.

--- js/modules/directives/d3.js
+++ js/modules/directives/d3.js
@@ -182,8 +182,8 @@ angular.module('krakenApp.Graph')
         });
 
         var zoom = d3.behavior.zoom()
-            .scaleExtent([0.5, 12])
-            .on("zoom", zoomed);
+          .scaleExtent([0.5, 12])
+          .on("zoom", zoomed);
 
         if (viewSettingsCache.translate && viewSettingsCache.scale) {
           zoom.translate(viewSettingsCache.translate).scale(viewSettingsCache.scale);
@@ -386,10 +386,11 @@ angular.module('krakenApp.Graph')
         }
 
         // create the div element that will hold the context menu
-        d3.selectAll('.d3-context-menu').data([1])
-            .enter()
-            .append('div')
-            .attr('class', 'd3-context-menu');
+        d3.selectAll('.d3-context-menu')
+          .data([1])
+          .enter()
+          .append('div')
+          .attr('class', 'd3-context-menu');
 
         // close menu
         d3.select('body').on('click.d3-context-menu', function() {
@@ -543,21 +544,23 @@ angular.module('krakenApp.Graph')
 
           d3.selectAll('.d3-context-menu').html('');
           var list = d3.selectAll('.d3-context-menu').append('ul');
-          list.selectAll('li').data(contextMenu).enter()
-              .append('li')
-              .html(function (d) {
-                return (typeof d.title === 'string') ? d.title : d.title(data);
-              })
-              .on('click', function (d, i) {
-                d.action(elm, data, index);
-                d3.select('.d3-context-menu').style('display', 'none');
-              });
+          list.selectAll('li')
+            .data(contextMenu)
+            .enter()
+            .append('li')
+            .html(function (d) {
+              return (typeof d.title === 'string') ? d.title : d.title(data);
+            })
+            .on('click', function (d, i) {
+              d.action(elm, data, index);
+              d3.select('.d3-context-menu').style('display', 'none');
+            });
 
           // display context menu
           d3.select('.d3-context-menu')
-              .style('left', (d3.event.pageX - 2) + 'px')
-              .style('top', (d3.event.pageY - 2) + 'px')
-              .style('display', 'block');
+            .style('left', (d3.event.pageX - 2) + 'px')
+            .style('top', (d3.event.pageY - 2) + 'px')
+            .style('display', 'block');
 
           d3.event.preventDefault();
         }
